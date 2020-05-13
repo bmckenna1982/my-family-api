9a92a43599cf37ae1ad2ad0bd42c03823d6b7860
bmckenna1982 <bmckenna1982@gmail.com>
2020-05-13 10:48:20 -0400

    created rewards endpoints and prepared for deployment

--- test/lists-endpoints.spec.js
+++ test/lists-endpoints.spec.js
@@ -9,7 +9,7 @@ describe('Lists endpoints', () => {
   before(() => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL,
+      connection: process.env.TEST_DATABASE_URL,
     })
     app.set('db', db)
   })
